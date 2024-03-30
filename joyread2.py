bf49548900f216de58bdbef4129677ffb32aeca5
BU Game Design <116972014+bugameclub@users.noreply.github.com>
2024-03-30 16:12:09 -0400

    Update joyread2.py
    Now works with joystick to create an emulated mouse.

--- joyread2.py
+++ joyread2.py
@@ -1,6 +1,9 @@
 import hid
 import pyautogui
 
+SPEED = 20
+
+
 #open device:
 
 h2 = hid.device()
@@ -9,12 +12,20 @@ h2.open(0x0079, 0x0006)
 
 try:
     while True:
-        d = h2.read(64)
+        d = h2.read(64, 100)
         if d:
             x = d[0]
             y = d[1]
+            x = (x - 127) // 127
+            y = (y - 127) // 127
+            x *= SPEED 
+            y *= SPEED
+            perint(x, y)
+            
 
-            pyautogui.move(x * 10, y * 10)
+            pyautogui.move(x, y)
+            time.sleep(0.05)
+            
 except KeyboardInterrupt:
     pass
 
